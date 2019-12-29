e1c90f0faf7e1e665b0c47c1cbd8ea5720e74a0f
Manuel Legrand <nuelegrand@gmail.com>
2019-12-28 22:35:53 -0500

    add code comments

--- app.js
+++ app.js
@@ -16,15 +16,18 @@ let phraseArray = getRandomPhraseAsArray(phrases);
 const heartsList = document.getElementsByClassName('tries');
 const retryButton = document.createElement('a');
 
+// create reset Button but not add it to the DOM yet 
 retryButton.textContent = 'Retry';
 retryButton.className = 'btn__reset';
 retryButton.id = 'retry';
 
+// function that picks a phrase at random from my list of phrases and creates the split array of letters
 function getRandomPhraseAsArray(arr){
     const index = Math.floor(Math.random()*(arr.length));
     return arr[index].split('');
 }
 
+// function that adds the phrase to display 
 function addPhraseToDisplay(arr){
     for (let i = 0; i < arr.length; i++){
         const letter = document.createElement('li');
@@ -38,6 +41,7 @@ function addPhraseToDisplay(arr){
     }   
 }
 
+// function that checks to see whether or not the letter clicked has a match
 function checkLetter(button){
     const phraseLetters = document.getElementsByClassName('letter');
     let correctLetter = null;
@@ -53,6 +57,7 @@ function checkLetter(button){
     return correctLetter;
 }
 
+// function that checks the score and can declare either a win or a loss 
 function checkWin(){
     const revealed = document.getElementsByClassName('show');
     const phraseLetters = document.getElementsByClassName('letter');
@@ -68,6 +73,8 @@ function checkWin(){
     }
 }
 
+// event listener for starting the game. 
+//reveals board, switches buttons, and adds a phrase 
 startButton.addEventListener('click', () => {
     overlay.style.display = 'none';
     overlay.appendChild(retryButton);
@@ -75,6 +82,7 @@ startButton.addEventListener('click', () => {
     addPhraseToDisplay(phraseArray);
 });
 
+// event listener for resetting the game 
 retryButton.addEventListener('click', () => {
     missed = 0;
     for (let i = 0; i < heartsList.length; i++){
@@ -96,6 +104,8 @@ retryButton.addEventListener('click', () => {
     overlay.style.display = 'none';
 });
 
+// event listener for selecting letters. checks for a match. 
+// if miss, adds to missed count and reflects on the screen. checks for a win.
 qwerty.addEventListener('click', (e) => {
     e.target.style.transition = '.5s';
     if (e.target.tagName === 'BUTTON'){
